4bfb6a48c0fe8e5ef31fd98b034253b0954021e5
yar <yaronatidim@gmail.com>
2019-03-13 01:18:08 +0200

    fix structure

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const jwt = require('jsonwebtoken');
 
+
+const DEFUALT_PROPS = {
+    jwt: {
+        options: {
+            algorithm: 'HS256',
+            expiresIn: '5m',
+            notBefore: undefined,
+            audience: undefined,
+            issuer: undefined,
+            jwtid: undefined,
+            subject: undefined,
+            noTimestamp: undefined,
+            header: undefined,
+            keyid: undefined,
+            mutatePayload: false
+        },
+        secret: null,
+    }
+};
+
+const props = {};
+
 class JWTExpress {
-    constructor(props) {
-        this.props = {}
+    init(props) {
+
     }
 
     sign(payload, options = {}, callback = null) {
@@ -18,5 +40,4 @@ class JWTExpress {
     }
 }
 
-
-module.exports = JWTExpress;
+module.exports = new JWTExpress();
