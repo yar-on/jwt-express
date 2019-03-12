201f407b31dce60a0277c39497d672db96368168
yar <yaronatidim@gmail.com>
2019-03-13 00:29:12 +0200

    create package structure

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 class JWTExpress {
+    constructor(props) {
+        this.props = {}
+    }
 
+    sign(payload, options = {}, callback = null) {
+
+    }
+
+    verify(token, options = {}, callback = null) {
+
+    }
+
+    middleware(req, res, next) {
+
+    }
 }
 
 
