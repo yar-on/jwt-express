a168358d18207cbfdf64bf272452230c5b8d552a
yar <yaronatidim@gmail.com>
2019-04-30 17:34:04 +0300

    support deep merge

--- libs/helpers/index.js
+++ libs/helpers/index.js
@@ -10,4 +10,24 @@ module.exports = class Helpers {
             return arr2.includes(item);
         })
     }
+
+    static deepMerge(...sources) {
+        let acc = {};
+        for (const source of sources) {
+            if (source instanceof Array) {
+                if (!(acc instanceof Array)) {
+                    acc = [];
+                }
+                acc = [...acc, ...source];
+            } else if (source instanceof Object) {
+                for (let [key, value] of Object.entries(source)) {
+                    if (value instanceof Object && key in acc) {
+                        value = this.deepMerge(acc[key], value);
+                    }
+                    acc = { ...acc, [key]: value }
+                }
+            }
+        }
+        return acc
+    }
 };
